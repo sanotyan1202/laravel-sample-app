f131414a837fdbe1e1176fd04d4cc04326856db3
sano <taikisano1202@gmail.com>
2023-03-12 13:04:27 +0900

    6章完了時点のプロジェクト

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BookPostRequest;
+use App\Http\Requests\BookPutRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
+use App\Models\Author;
 use App\Models\Book;
 use App\Models\Category;
 
@@ -26,13 +29,9 @@ class BookController extends Controller
                 ->header('Content-Encoding', 'UTF-8');
     }
 
-    public function show(string $id): Book
+    public function show(Book $book): View
     {
-        // 書籍を一件取得
-        $book = Book::findOrFail($id);
-
-        // 取得した書籍をレスポンスとして返す
-        return $book;
+        return view('admin/book/show', compact('book'));
     }
 
     public function create(): View
@@ -40,10 +39,11 @@ class BookController extends Controller
         // ビューにカテゴリ一覧を表示するために全件取得
         $categories = Category::all();
 
-        // ビューオブジェクトを返す
-        return view('admin/book/create', [
-            'categories' => $categories
-        ]);
+        // 著者一覧を表示するために全件取得
+        $authors = Author::all();
+
+        return view('admin/book/create', 
+            compact('categories', 'authors'));
     }
 
     public function store(BookPostRequest $request): RedirectResponse
@@ -56,11 +56,62 @@ class BookController extends Controller
         $book->title = $request->title;
         $book->price = $request->price;
 
-        // 保存
-        $book->save();
+        DB::transaction(function() use($book, $request) {
 
+            // 保存
+            $book->save();
+
+            // 著者書籍テーブルを登録
+            $book->authors()->attach($request->author_ids);
+        });
+            
         // 登録完了後book.indexにリダイレクトする
         return redirect(route('book.index'))
         ->with('message', $book->title . 'を追加しました。');
+    }
+
+    public function edit(Book $book): View
+    {
+        // カテゴリ一覧を表示するために全件取得
+        $categories = Category::all();
+
+        // 著者一覧を表示するために全件取得
+        $authors = Author::all();
+
+        // 書籍に紐づく著者IDの一覧を取得
+        $authorIds = $book->authors()->pluck('id')->all();
+
+        return view('admin/book/edit', 
+            compact('book', 'categories', 'authors', 'authorIds'));
+    }
+
+    public function update(BookPutRequest $request, Book $book):
+    RedirectResponse
+    {
+        // リクエストオブジェクトからパラメータを取得する
+        $book->category_id = $request->category_id;
+        $book->title = $request->title;
+        $book->price = $request->price;
+
+        DB::transaction(function() use($book, $request) {
+
+            // 更新
+            $book->update();
+
+            // 書籍と著者の関連付けを更新する
+            $book->authors()->sync($request->author_ids);
+        });
+
+        return redirect(route('book.index'))
+            ->with('message', $book->title . 'を変更しました。');
+    }
+
+    public function destroy(Book $book): RedirectResponse
+    {
+        // 削除
+        $book->delete();
+        
+        return redirect(route('book.index'))
+            ->with('message', $book->title . 'を削除しました。');
     }
 }
--- lang/ja/validation.php
+++ lang/ja/validation.php
@@ -19,5 +19,7 @@ return [
         'category_id' => 'カテゴリ',
         'title' => 'タイトル',
         'price' => '価格',
+        'author_ids' => '著者',
+        'author_ids.*' => '著者',    
     ],
 ];
